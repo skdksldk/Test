f578e69b17d8758cee38d04c22947caa9269daf7
skdksldk <tnwns1485@naver.com>
2022-03-08 21:19:26 +0900

    Add back menu to Header

--- src/store/MovieStore.js
+++ src/store/MovieStore.js
@@ -4,16 +4,24 @@ import _ from 'lodash';
 
 
 const store = observable({
-    movieList: [], 
-    isMovieLoded: false, 
-    sortMethod: '', 
-    sortMethodName: '현재 상영중인 영화', 
-    movieBg: '', 
-    isMovieSelected: false, 
-    selectedMovie: [], 
+    movieList: [],
+    isMovieLoded: false,
+    sortMethod: '',
+    sortMethodName: '현재 상영중인 영화',
+    movieBg: '',
+    isMovieSelected: false,
+    selectedMovie: [],
+    searchWord: '',
+    searchWordFix: '',
+    recommendedMovie: [],
+    recommendCount: 3,
+    movieTrailer: [],
+    movieTrailerKey: '',
+    isExisTrailer: false,
+    isShowTrailer: false,
 
     getApi(sortPram) {
-        
+
         let SORT = '';
         const NOW_PLAYING = '/movie/now_playing';
         const TRENDING = '/trending/movie/week';
@@ -26,23 +34,23 @@ const store = observable({
         const LANGUAGE_KR = '&language=ko-KR';
 
         if (sortPram == '0') {
-            
+
             SORT = NOW_PLAYING;
             this.sortMethodName = '현재 상영중인 영화';
         } else if (sortPram == '1') {
-            
+
             SORT = TRENDING;
             this.sortMethodName = '최근 인기있는 영화'
         } else if (sortPram == '2') {
-            
+
             SORT = TOP_RATED;
             this.sortMethodName = '최근 평점높은 영화'
         } else if (sortPram == '3') {
-            
+
             SORT = UPCOMING;
             this.sortMethodName = '최근 개봉 & 예정 영화'
         } else if (sortPram == '4') {
-            
+
             SORT = SEARCH;
             this.sortMethodName = this.searchWordFix + ' 키워드로 검색한 영화'
 
@@ -57,7 +65,7 @@ const store = observable({
     },
 
     async getMovies(sortPram) {
-    
+
         const movies = await this.getApi(sortPram);
         if (movies.results.length <= 0) {
             this.setSearchFailed();
@@ -70,44 +78,61 @@ const store = observable({
         }
     },
     setMovie(movieData) {
-    
+
         this.movieList = movieData
     },
     checkMovieLoad(movieObj) {
-    
+
         if (!_.isEmpty(movieObj)) return this.isMovieLoded = true;
         else return false;
     },
     setSearchSuccess() {
-        
+
         this.isSuccessSearch = true;
     },
-    setSearchKeyword(keyword){
+    setSearchKeyword(keyword) {
 
         this.searchWord = keyword;
     },
-    setKeywordFix(){
-    
+    setKeywordFix() {
+
         this.searchWordFix = this.searchWord;
     },
     checkMovieLoad(movieObj) {
-        
+
         if (!_.isEmpty(movieObj)) return this.isMovieLoded = true;
         else return false;
     },
     changeMovieBg(theMovieBg) {
-        
+
         this.movieBg = theMovieBg;
     },
-    movieSelectToggle(){
+    movieSelectToggle() {
 
-        if ( !this.isMovieSelected ) this.isMovieSelected = !this.isMovieSelected;
+        if (!this.isMovieSelected) this.isMovieSelected = !this.isMovieSelected;
     },
-    setBgRestore(){
-    
+    setBgRestore() {
+
         this.changeMovieBg(this.selectedMovie.backdrop_path);
     },
+    backHome() {
 
+        this.isMovieSelected = false;
+    },
+    setClearSelectedMovie() {
+
+        this.selectedMovie = {};
+    },
+    setRecommendCountRestore() {
+        this.recommendCount = 3;
+    },
+    setCastCountRestore() {
+        this.castCount = 3;
+    },
+    setHideTrailer() {
+        this.isShowTrailer = false;
+    }
+   
 })
 
 export default store; 
